5c2956c407d22b1c623c4013bba44a599116b737
Paulina <paulinalourenco1@gmail.com>
2024-05-04 21:25:55 +0200

    looping through pages using next page token

--- Python_API/youtube_api.py
+++ Python_API/youtube_api.py
@@ -39,59 +39,69 @@ def api_request_channel():
         'video_count': channel_response['items'][0]['statistics']['videoCount']
     }
      #defining the service:
-    request = youtube.playlistItems().list(
-        part = 'contentDetails',
-        playlistId = channel_details['uploads_playlist_id'],
-        maxResults = '2'
-    )
-    playlistItems_response = request.execute()
-
-    next_page_Token = playlistItems_response['nextPageToken']
-    
-
-    # STOPPED HERE
-    #iterate through the items - ADD THE TOKEN PAGE TO ITERATE THROU MORE THAN 50
-    i = 0
-    video_id_list = []
-    for k in playlistItems_response['items']:
-        video_id = playlistItems_response['items'][i]['contentDetails']['videoId']
-        video_id_list.append(video_id)
-        i += 1
-    # print(video_id_list)
-
-
-    #defining the service:
-    request = youtube.videos().list(
-        part = 'snippet, contentDetails, statistics',
-        id = video_id_list
-    )
-    # snippet - contains title
-    # contentDetails - contains lenght
-    # statistics - contains views, likes, comments
-
     vidoes_details_list = []
-    video_response = request.execute()
-    for video in video_response['items']:
-        video_duration = video['contentDetails']['duration']
-        video_duration_hours =  (re.findall('[\d]+(?=H)', video_duration)[0] if re.findall('[\d]+(?=H)', video_duration) else 0 )
-        video_duration_minutes =  (re.findall('[\d]+(?=M)', video_duration)[0] if re.findall('[\d]+(?=M)', video_duration) else 0)
-        video_duration_seconds =  (re.findall('[\d]+(?=S)', video_duration)[0] if re.findall('[\d]+(?=S)', video_duration) else 0)
-        video_published_date = re.findall('[\d-]+(?=T)',video['snippet']['publishedAt'])[0]
-        video_published_time = re.findall('[\d:]+(?=Z)',video['snippet']['publishedAt'])[0]
-        video_details = {
-            'video_title': video['snippet']['title'],
-            'video_published_date': video_published_date,
-            'video_published_time': video_published_time,
-            'video_duration': str(video_duration_hours)+ ":"+ str(video_duration_minutes) + ':' + str(video_duration_seconds),
-            'video_views': video['statistics']['viewCount'],
-            'video_likes': video['statistics']['likeCount'],
-            'video_comments': video['statistics']['commentCount']
-        }
-        print(video_details)
-        print()
-        vidoes_details_list.append(video_details)
+    next_Page_Token = None
+    while True:
+        request = youtube.playlistItems().list(
+            part = 'contentDetails',
+            playlistId = channel_details['uploads_playlist_id'],
+            maxResults = '50',
+            pageToken = next_Page_Token
+        )
+        playlistItems_response = request.execute()
 
 
+        
+
+        i = 0
+        video_id_list = []
+        for k in playlistItems_response['items']:
+            video_id = playlistItems_response['items'][i]['contentDetails']['videoId']
+            video_id_list.append(video_id)
+            i += 1
+        # print(video_id_list)
+
+
+        #defining the service:
+        request = youtube.videos().list(
+            part = 'snippet, contentDetails, statistics',
+            id = video_id_list
+        )
+        # snippet - contains title
+        # contentDetails - contains lenght
+        # statistics - contains views, likes, comments
+
+
+        video_response = request.execute()
+        for video in video_response['items']:
+            video_duration = video['contentDetails']['duration']
+            video_duration_hours =  (re.findall('[\d]+(?=H)', video_duration)[0] if re.findall('[\d]+(?=H)', video_duration) else 0 )
+            video_duration_minutes =  (re.findall('[\d]+(?=M)', video_duration)[0] if re.findall('[\d]+(?=M)', video_duration) else 0)
+            video_duration_seconds =  (re.findall('[\d]+(?=S)', video_duration)[0] if re.findall('[\d]+(?=S)', video_duration) else 0)
+            video_published_date = re.findall('[\d-]+(?=T)',video['snippet']['publishedAt'])[0]
+            video_published_time = re.findall('[\d:]+(?=Z)',video['snippet']['publishedAt'])[0]
+            try:
+                video_comments = video['statistics']['commentCount']
+            except:
+                video_comments = 0
+            video_details = {
+                'video_title': video['snippet']['title'],
+                'video_published_date': video_published_date,
+                'video_published_time': video_published_time,
+                'video_duration': str(video_duration_hours)+ ":"+ str(video_duration_minutes) + ':' + str(video_duration_seconds),
+                'video_views': video['statistics']['viewCount'],
+                'video_likes': video['statistics']['likeCount'],
+                'video_comments': video_comments
+            }
+            # print(video_details)
+            # print()
+            vidoes_details_list.append(video_details)
+
+        next_Page_Token = playlistItems_response.get('nextPageToken')
+        if not next_Page_Token:
+            break
+ 
+    print(len(vidoes_details_list))        
 
 if __name__ == '__main__':
     get_videos()
