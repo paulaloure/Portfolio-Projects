03d8dc4dfacfb509666a80163371334a3da48507
Paulina <paulinalourenco1@gmail.com>
2024-06-25 20:22:10 +0200

    added a step to load data into database

--- Python_API/main.py
+++ Python_API/main.py
@@ -1,8 +1,12 @@
 import time
+import os
 import pandas as pd
 
+
+from dotenv import load_dotenv
 from datetime import datetime
 from youtube_api import get_videos
+from sqlalchemy import create_engine
 
 #Creating file names that will be used to save excel files with data
 channel_file_name = "Youtube_channel_details_" + datetime.now().strftime("%Y-%m-%d_%I-%M") + ".xlsx"
@@ -21,23 +25,50 @@ def convert_to_df(video_data):
     videos_table = pd.DataFrame(video_data)
     return videos_table
 
-#exporting channel data
-def export_videos():
-    start_time = time.time()
-    #total time that the function run:
+
+#loading DB password that is saved in .env file
+def configure():
+    load_dotenv()
+
+#connecting to the database:
+def connect_to_db():
+    configure()
+    user_name = 'postgres'
+    host = 'localhost'
+    database = 'youtube_channel'
+    port = '5432'
+    password = os.getenv('DB_password')
+    engine = create_engine(f'postgresql://{user_name}:{password}@{host}:{port}/{database}')
+    print("Successfully connected to the database")
+    return engine
+
+#exporting channel data and loading to database
+def save_data(engine):
+    #getting the data:
     channel_data, video_data = get_videos()
     print("Saving as dataframes")
     df_channel = convert_to_df([channel_data])
     df_videos = convert_to_df(video_data)
+    #saving to excel:
     print("Saving to excel")
     df_channel.to_excel(channel_file_name)
     df_videos.to_excel(videos_file_name)
+    #loading into database:
+    print("Saving to database")
+    df_channel.to_sql('channel_details', engine, if_exists='append', index=False)
+    df_videos.to_sql('videos_details', engine, if_exists='replace', index=False)
+
+
+
+def export_videos():
+    #tracking the time the tasks take to complete:
+    start_time = time.time()
+    engine = connect_to_db()
+    save_data(engine)
     stop_time = time.time()
     duration = stop_time - start_time
     print("Task completed successfully")
     print(change_time_format(duration)) #printing how much time task took to complete
-
-
 
 
 if __name__ == '__main__': 
